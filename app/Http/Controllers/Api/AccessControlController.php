4bfad90f415e0d20cd8911aef397eb7779078a53
Marco Moraes <marcoantoniomoraes760@gmail.com>
2024-08-21 10:38:23 -0300

    refactor: Ajustar método para consultar status da assinatura de um cliente

--- app/Http/Controllers/Api/AccessControlController.php
+++ app/Http/Controllers/Api/AccessControlController.php
@@ -12,8 +12,20 @@ class AccessControlController extends Controller
     {
         $customer = Cliente::where('cpf_cnpj', $customer_cnpj_cpf)->first();
         if (isset($customer) && $customer->situacao == 'Ativa') {
-            $dueDate = $customer->installments->where('status', 'atrasado')->first()->data_vencimento;
-            return response()->json(['customer' => $customer->nome_fantasia, 'dueDate' => $dueDate], 200);
+            $installments = $customer->installments;
+            $expiredInstallments = $installments->where('status', 'atrasado');
+            if (count($expiredInstallments) > 0) {
+                $due = [
+                    'expires' => true,
+                    'expired_on' => $expiredInstallments->first()->data_vencimento
+                ];
+            } else {
+                $due = [
+                    'expires' => false,
+                    'expires_in' => $installments->where('status', 'pendente')->first()->data_vencimento
+                ];
+            }
+            return response()->json(['customer' => $customer->nome_fantasia, 'due' => $due], 200);
         }
         return response()->json(['message' => 'Cliente não encontrado ou inativo para cobrança!'], 404);
     }
